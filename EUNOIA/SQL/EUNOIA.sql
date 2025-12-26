48ebdac7d1b218fdf4a875fba9dbb253ba3d5edd
Woojin CHO <littletree0330@gmail.com>
2025-12-26 22:35:32 +0900

    ✨ [EUNOIA]SQL문 수정

--- EUNOIA/SQL/EUNOIA.sql
+++ EUNOIA/SQL/EUNOIA.sql
@@ -23,3 +23,12 @@ select * from member;
 select * from EMOTION_ENTRY;
 select * from EMOTION_ANALYSIS;
 select * from ANALYSIS_WARM_MESSAGE;
+
+-- 1. 따뜻한 말 테이블 먼저 삭제 (가장 종속된 테이블)
+DELETE FROM ANALYSIS_WARM_MESSAGE;
+
+-- 2. 감정 분석 테이블 삭제
+DELETE FROM EMOTION_ANALYSIS;
+
+-- 3. 감정 글 테이블 삭제
+DELETE FROM EMOTION_ENTRY;
